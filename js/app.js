92b6fc1b34829267e0f0cb29e07c4fb191ab12b5
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-01-26 18:10:54 -0300

    mudança no layout, interação com modal no adminEdit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 //Painel usuário ou admin
-const estado = document.querySelector(".estado");
 const forms = document.querySelectorAll(".panel");
 const sections = document.querySelectorAll(".section");
 
+//Modal
+const modalAdminEdit = document.querySelector(".modalAdminEdit");
+
 //Inputs
-const inputs = document.querySelectorAll(".input-group");
-const select = document.querySelector("#select");
+const userPanel = document.querySelector("#userPanel");
+const adminPanel = document.querySelector("#adminPanel");
 const qtdComprar = document.querySelector("#qtdComprar");
 const btnComprar = document.querySelector("#btnComprar");
 const novoIngresso = document.querySelector("#novoIngresso");
 const qtdAdd = document.querySelector("#qtdAdd");
 const btnAdd = document.querySelector("#btnAdd");
+
+const body = document.body;
 
 function lockUnlockBtn(input1, input2, btn) {
   if (input1.value !== "" && input2.value > 0) {
@@ -22,32 +26,64 @@ function lockUnlockBtn(input1, input2, btn) {
   }
 }
 
-inputs.forEach((elemento) => {
-  elemento.addEventListener("input", (e) => {
-    if (e.target instanceof HTMLSelectElement) {
+function toggle(alvo) {
+  if (alvo instanceof NodeList) {
+    for (let i = 0; i < alvo.length; i++) {
+      alvo[i].classList.toggle("hide");
+    }
+    return;
+  }
+  alvo.classList.toggle("hide");
+}
+
+document.addEventListener("click", (e) => {
+  const target = e.target;
+
+  switch (target.id || target.classList[0]) {
+    // Painel do usuário
+    case "adminPanel":
+    case "userPanel":
+      toggle(userPanel);
+      toggle(adminPanel);
+      toggle(forms);
+      toggle(sections);
+      break;
+    case "select":
       console.log(e.target.value);
-    }
+      break;
+    case "qtdComprar":
+      target.addEventListener("input", () => {
+        lockUnlockBtn("", qtdComprar, btnComprar);
+      });
+      break;
+    case "btnComprar":
+      console.log("Compra realizada");
+      break;
 
-    if (e.target == qtdComprar) {
-      lockUnlockBtn("", qtdComprar, btnComprar);
-    }
-    lockUnlockBtn(novoIngresso, qtdAdd, btnAdd);
-  });
-});
-
-estado.addEventListener("click", (e) => {
-  const childrens = estado.children;
-  const alvo = e.target;
-
-  for (let i = 0; i < childrens.length; i++) {
-    if (childrens[i].contains(alvo)) {
-      childrens[i].classList.toggle("hide");
-      forms[i].classList.toggle("hide");
-      sections[i].classList.toggle("hide");
-    } else {
-      childrens[i].classList.toggle("hide");
-      forms[i].classList.toggle("hide");
-      sections[i].classList.toggle("hide");
-    }
+    // Painel do admin
+    case "novoIngresso":
+    case "qtdAdd":
+      target.addEventListener("input", () => {
+        lockUnlockBtn(novoIngresso, qtdAdd, btnAdd);
+      });
+      break;
+    case "btnAdd":
+      console.log("Adicionou novo ingresso");
+      break;
+    case "edit":
+      toggle(modalAdminEdit);
+      document.body.classList.add("modal-block-body");
+      break;
+    case "delete":
+      console.log("Deletar");
+      break;
+    case "modalClose":
+      modalAdminEdit.classList.add("fade-out");
+      setTimeout(() => {
+        toggle(modalAdminEdit);
+        modalAdminEdit.classList.remove("fade-out");
+        document.body.classList.remove("modal-block-body");
+      }, 200);
+      break;
   }
 });
