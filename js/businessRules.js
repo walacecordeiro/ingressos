1f3a3d63981bdc487adabe01226f358cdefc7f9f
Walace Cordeiro dos Santos <walacecordeirodossantos@gmail.com>
2024-01-30 14:55:19 -0300

    progresso nas regras de negócio
    alteração do código para ingles; adicional de interação para admins

--- js/businessRules.js
+++ js/businessRules.js
@@ -1,9 +1,76 @@
-export class Ingressos {
+export class Tickets {
   constructor() {
-    this.lista = [
-      { tipo: "Pista", quantidade: 100 },
-      { tipo: "Cadeira Superior", quantidade: 200 },
-      { tipo: "Cadeira Inferior", quantidade: 400 },
+    this.list = [
+      { type: "Pista", amount: 100 },
+      { type: "Cadeira Superior", amount: 200 },
+      { type: "Cadeira Inferior", amount: 400 },
     ];
+    this.selectedOption = this.list[0].type;
+    this.qttDemanded = 0;
+  }
+
+  purchase() {
+    const selectedOption = this.list.find(
+      (ingresso) => ingresso.type === this.selectedOption
+    );
+
+    const selectedOptionSP =
+      selectedOption.amount > 1 ? "disponíveis" : "disponível";
+
+    if (selectedOption && this.qttDemanded > 0 && !isNaN(this.qttDemanded)) {
+      if (selectedOption.amount <= 0) {
+        alert("Este ingresso não está mais disponível");
+      } else if (
+        selectedOption.amount > 0 &&
+        this.qttDemanded > selectedOption.amount
+      ) {
+        alert(
+          `Você deseja comprar ${
+            this.qttDemanded
+          } tickets para ${selectedOption.type.toLocaleLowerCase()} mas temos apenas ${
+            selectedOption.amount
+          } ${selectedOptionSP}`
+        );
+      } else {
+        selectedOption.amount -= this.qttDemanded;
+        alert("Compra realizada com sucesso!");
+      }
+    } else {
+      console.log("Adicione uma quantidade ao pedido");
+    }
+  }
+
+  addTicketToList(
+    type,
+    amount,
+    select,
+    options,
+    userHTML,
+    adminHTML
+  ) {
+    this.list.push({
+      type: type.value,
+      amount: parseInt(amount.value),
+    });
+
+    if (this.list.length > options.length) {
+      const novoOption = document.createElement("option");
+      novoOption.textContent = type.value;
+      select.appendChild(novoOption);
+
+      const novoLi = document.createElement("li");
+      novoLi.innerHTML = `${type.value}<span>${amount.value}</span>`;
+      userHTML.appendChild(novoLi);
+
+      const novoLiAdmin = document.createElement("li");
+      novoLiAdmin.innerHTML = `
+      Tipo:<span>${type.value}</span>Qantidade:<span>${amount.value}</span>
+      <div class="icons">
+        <i class="edit fa-solid fa-pen-to-square"></i>
+        <i class="delete fa-solid fa-trash"></i>
+      </div>
+    `;
+      adminHTML.appendChild(novoLiAdmin);
+    }
   }
 }
--- js/interaction.js
+++ js/interaction.js
@@ -1,3 +1,6 @@
+import { Tickets } from "./businessRules.js";
+const tickets = new Tickets();
+
 function lockUnlockBtn(input1, input2, btn) {
   if (input1.value !== "" && input2.value > 0) {
     btn.classList.remove("blockBtn");
@@ -18,13 +21,62 @@ function toggle(alvo) {
   alvo.classList.toggle("hide");
 }
 
-function modalClose(modalAdminEdit) {
-  modalAdminEdit.classList.add("fade-out");
+function modalClose(modal) {
+  modal.classList.add("fade-out");
   setTimeout(() => {
-    toggle(modalAdminEdit);
-    modalAdminEdit.classList.remove("fade-out");
+    toggle(modal);
+    modal.classList.remove("fade-out");
     document.body.classList.remove("modal-block-body");
   }, 200);
+}
+
+function newLi() {
+  const newLi = document.createElement("li");
+  return newLi;
+}
+
+export function initialContent({ select, userHTML, adminHTML, list }) {
+  for (let i = 0; i < select.children.length && i < list.length; i++) {
+    const newOption = document.createElement("option");
+    const spanList = document.getElementsByTagName("span");
+
+    if (select.children.length < list.length) {
+      select.append(newOption);
+      userHTML.append(newLi());
+      adminHTML.append(newLi());
+    }
+
+    select[i].textContent = list[i].type;
+    userHTML.children[i].innerHTML = ` ${list[i].type}
+      <span>${(spanList.textContent = tickets.list[i].amount)}</span>`;
+
+    adminHTML.children[i].innerHTML = `
+      Tipo:<span>${list[i].type}</span>Qantidade:<span>${tickets.list[i].amount}</span>
+      <div class="icons">
+        <i class="edit fa-solid fa-pen-to-square"></i>
+        <i class="delete fa-solid fa-trash"></i>
+      </div>
+    `;
+  }
+}
+
+function updateContent(list) {
+  const userHTML = document.querySelector("#userHTML");
+  const spanList = document.getElementsByTagName("span");
+  const adminHTML = document.querySelector("#adminHTML");
+
+  for (let i = 0; i < list.length; i++) {
+    userHTML.children[i].innerHTML = ` ${list[i].type}
+  <span>${(spanList.textContent = list[i].amount)}</span>`;
+
+    adminHTML.children[i].innerHTML = `
+  Tipo:<span>${list[i].type}</span>Qantidade:<span>${list[i].amount}</span>
+  <div class="icons">
+  <i class="edit fa-solid fa-pen-to-square"></i>
+  <i class="delete fa-solid fa-trash"></i>
+  </div>
+  `;
+  }
 }
 
 export function handleEvents(
@@ -32,13 +84,16 @@ export function handleEvents(
   {
     forms,
     sections,
-    modalAdminEdit,
+    modal,
     userPanel,
     adminPanel,
     qtdComprar,
     btnComprar,
-    novoIngresso,
+    newTicket,
     qtdAdd,
+    select,
+    option,
+    userHTML,
   }
 ) {
   const target = e.target;
@@ -53,29 +108,38 @@ export function handleEvents(
       toggle(sections);
       break;
     case "select":
-      console.log(e.target.value);
+      tickets.selectedOption = e.target.value;
       break;
     case "qtdComprar":
       target.addEventListener("input", () => {
+        tickets.qttDemanded = parseInt(e.target.value);
         lockUnlockBtn("", qtdComprar, btnComprar);
       });
       break;
     case "btnComprar":
-      console.log("Compra realizada");
+      tickets.purchase();
+      updateContent(tickets.list);
       break;
 
     // Painel do admin
-    case "novoIngresso":
+    case "newTicket":
     case "qtdAdd":
       target.addEventListener("input", () => {
-        lockUnlockBtn(novoIngresso, qtdAdd, btnAdd);
+        lockUnlockBtn(newTicket, qtdAdd, btnAdd);
       });
       break;
     case "btnAdd":
-      console.log("Adicionou novo ingresso");
+      tickets.addTicketToList(
+        newTicket,
+        qtdAdd,
+        select,
+        option,
+        userHTML,
+        adminHTML
+      );
       break;
     case "edit":
-      toggle(modalAdminEdit);
+      toggle(modal);
       document.body.classList.add("modal-block-body");
       break;
     case "updateIngresso":
@@ -85,14 +149,14 @@ export function handleEvents(
       });
       break;
     case "btnUpdate":
-      modalClose(modalAdminEdit);
+      modalClose(modal);
       console.log("Atualizado com sucesso");
       break;
     case "delete":
       console.log("Deletar");
       break;
     case "modalClose":
-      modalClose(modalAdminEdit);
+      modalClose(modal);
       break;
   }
 }
